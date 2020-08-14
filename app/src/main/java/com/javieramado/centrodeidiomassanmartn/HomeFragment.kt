a7ee7ad9283914de80818516fa0c6557596d2217
Javier Amado <JavierAmado7@gmail.com>
2020-08-14 15:46:18 +0200

    Mejora de imágenes

--- app/src/main/java/com/javieramado/centrodeidiomassanmartn/HomeFragment.kt
+++ app/src/main/java/com/javieramado/centrodeidiomassanmartn/HomeFragment.kt
@@ -9,7 +9,7 @@ import com.synnapps.carouselview.CarouselView
 
 class HomeFragment : Fragment() {
     private val fotos = intArrayOf(
-            R.drawable.photo_5, R.drawable.photo_1, R.drawable.photo_2, R.drawable.photo_3, R.drawable.photo_4, R.drawable.photo_6,
+            R.drawable.bannercentroidiomas, R.drawable.photo_1, R.drawable.photo_2, R.drawable.photo_3, R.drawable.photo_4, R.drawable.photo_6,
             R.drawable.photo_7, R.drawable.photo_8, R.drawable.photo_9, R.drawable.photo_10, R.drawable.photo_11, R.drawable.photo_12,
             R.drawable.photo_13, R.drawable.photo_14, R.drawable.photo_15, R.drawable.photo_16, R.drawable.photo_17, R.drawable.photo_18, R.drawable.photo_19,
             R.drawable.photo_20, R.drawable.photo_21, R.drawable.photo22
